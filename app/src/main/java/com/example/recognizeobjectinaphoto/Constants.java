04c52cccc7c61dfe4fc5c64c0accfbee4ad039b3
v.fedotova <biersack041299@gmail.com>
2022-06-01 18:10:26 +0300

    'AddSplashScreen'

---
+++ app/src/main/java/com/example/recognizeobjectinaphoto/Constants.java
@@ -0,0 +1,5 @@
+package com.example.recognizeobjectinaphoto;
+
+public class Constants {
+    public static final int SPLASH_DISPLAY_LENGTH = 2000;
+}
---
+++ app/src/main/java/com/example/recognizeobjectinaphoto/WelcomeActivity.java
@@ -0,0 +1,42 @@
+package com.example.recognizeobjectinaphoto;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.os.Handler;
+import com.google.android.material.textview.MaterialTextView;
+import static com.example.recognizeobjectinaphoto.Constants.SPLASH_DISPLAY_LENGTH;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    MaterialTextView loadingText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        initTextView();
+
+        new Handler().postDelayed(() -> {
+
+            Intent mainIntent = new Intent(WelcomeActivity.this, MainActivity.class);
+            WelcomeActivity.this.startActivity(mainIntent);
+            WelcomeActivity.this.finish();
+
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+
+    public void initTextView(){
+
+        loadingText = findViewById(R.id.loading_text_view);
+        loadingText.setTypeface(Typeface.createFromAsset(getAssets(),getString(R.string.elsie_regular)));
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        super.onBackPressed();
+    }
+}
